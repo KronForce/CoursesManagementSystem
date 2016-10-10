c1d8c94f90a10186830363f89c6c4d3fb8033fbc
KronForce <mikesosnin@outlook.com>
2016-10-10 14:02:11 +0300

    Класс получил поля для упрощения работы со шрифтом.

--- src/view/GroupsManagement.java
+++ src/view/GroupsManagement.java
@@ -21,23 +21,30 @@ public class GroupsManagement extends BaseFrame {
         JPanel physPanel = new JPanel();
         JPanel russianPanel = new JPanel();
 
+        String fontName = "Times New Roman";
+        int fonrType = 2;
+        int fontSize = 15;
+
+
         JLabel mathLabel = new JLabel();
-        mathLabel.setPreferredSize(new Dimension(70, 50));
+        mathLabel.setPreferredSize(new Dimension(90, 50));
         mathLabel.setBackground(Color.black);
         mathLabel.setText("Математика");
+        mathLabel.setFont(new Font(fontName,fonrType,fontSize));
         mathPanel.add(mathLabel);
 
         JLabel physLabel = new JLabel();
-        physLabel.setPreferredSize(new Dimension(70, 50));
-        //physPanel.setLayout(new BoxLayout());
+        physLabel.setPreferredSize(new Dimension(75, 55));
         physLabel.setBackground(Color.black);
         physLabel.setText("    Физика  ");
+        physLabel.setFont(new Font(fontName,fonrType,fontSize));
         physPanel.add(physLabel);
 
         JLabel russianLabel = new JLabel();
         russianLabel.setPreferredSize(new Dimension(100, 50));
         russianLabel.setBackground(Color.black);
         russianLabel.setText("  Русский язык");
+        russianLabel.setFont(new Font(fontName,fonrType,fontSize));
         russianPanel.add(russianLabel);
 
 
@@ -48,6 +55,7 @@ public class GroupsManagement extends BaseFrame {
         //------------------------------------------------
 
         JButton ma1Button = new JButton("MA1");
+        ma1Button.setFont(new Font(fontName,fonrType,fontSize));
         ma1Button.setPreferredSize(new Dimension(400, 50));
         ma1Button.addActionListener(new ActionListener() {
             @Override
@@ -57,6 +65,7 @@ public class GroupsManagement extends BaseFrame {
         });
 
         JButton ma2Button = new JButton("MA2");
+        ma2Button.setFont(new Font(fontName,fonrType,fontSize));
         ma2Button.setPreferredSize(new Dimension(400, 50));
         ma2Button.addActionListener(new ActionListener() {
             @Override
@@ -66,6 +75,7 @@ public class GroupsManagement extends BaseFrame {
         });
 
         JButton mbButton = new JButton("MB");
+        mbButton.setFont(new Font(fontName,fonrType,fontSize));
         mbButton.setPreferredSize(new Dimension(400, 50));
         mbButton.addActionListener(new ActionListener() {
             @Override
@@ -75,6 +85,7 @@ public class GroupsManagement extends BaseFrame {
         });
 
         JButton mcButton = new JButton("MC");
+        mcButton.setFont(new Font(fontName,fonrType,fontSize));
         mcButton.setPreferredSize(new Dimension(400, 50));
         mcButton.addActionListener(new ActionListener() {
             @Override
@@ -84,6 +95,7 @@ public class GroupsManagement extends BaseFrame {
         });
 
         JButton msButton = new JButton("MS");
+        msButton.setFont(new Font(fontName,fonrType,fontSize));
         msButton.setPreferredSize(new Dimension(400, 50));
         msButton.addActionListener(new ActionListener() {
             @Override
@@ -92,6 +104,7 @@ public class GroupsManagement extends BaseFrame {
             }
         });
         JButton moButton = new JButton("MO");
+        moButton.setFont(new Font(fontName,fonrType,fontSize));
         moButton.setPreferredSize(new Dimension(400, 50));
         moButton.addActionListener(new ActionListener() {
             @Override
@@ -100,6 +113,7 @@ public class GroupsManagement extends BaseFrame {
             }
         });
         JButton mo2Button = new JButton("MO2");
+        mo2Button.setFont(new Font(fontName,fonrType,fontSize));
         mo2Button.setPreferredSize(new Dimension(400, 50));
         mo2Button.addActionListener(new ActionListener() {
             @Override
@@ -119,6 +133,7 @@ public class GroupsManagement extends BaseFrame {
         //------------------------------------------------
 
         JButton fa1Button = new JButton("ФА1");
+        fa1Button.setFont(new Font(fontName,fonrType,fontSize));
         fa1Button.setPreferredSize(new Dimension(400, 50));
         fa1Button.addActionListener(new ActionListener() {
             @Override
@@ -126,7 +141,9 @@ public class GroupsManagement extends BaseFrame {
                 new GroupWindow(fa1Button.getText());
             }
         });
+
         JButton fa2Button = new JButton("ФА2");
+        fa2Button.setFont(new Font(fontName,fonrType,fontSize));
         fa2Button.setPreferredSize(new Dimension(400, 50));
         fa2Button.addActionListener(new ActionListener() {
             @Override
@@ -134,7 +151,9 @@ public class GroupsManagement extends BaseFrame {
                 new GroupWindow(fa2Button.getText());
             }
         });
+
         JButton fb1Button = new JButton("ФВ1");
+        fb1Button.setFont(new Font(fontName,fonrType,fontSize));
         fb1Button.setPreferredSize(new Dimension(400, 50));
         fb1Button.addActionListener(new ActionListener() {
             @Override
@@ -142,7 +161,9 @@ public class GroupsManagement extends BaseFrame {
                 new GroupWindow(fb1Button.getText());
             }
         });
+
         JButton fc1Button = new JButton("ФС1");
+        fc1Button.setFont(new Font(fontName,fonrType,fontSize));
         fc1Button.setPreferredSize(new Dimension(400, 50));
         fc1Button.addActionListener(new ActionListener() {
             @Override
@@ -150,7 +171,9 @@ public class GroupsManagement extends BaseFrame {
                 new GroupWindow(fc1Button.getText());
             }
         });
+
         JButton fr1Button = new JButton("ФР1");
+        fr1Button.setFont(new Font(fontName,fonrType,fontSize));
         fr1Button.setPreferredSize(new Dimension(400, 50));
         fr1Button.addActionListener(new ActionListener() {
             @Override
@@ -158,7 +181,9 @@ public class GroupsManagement extends BaseFrame {
                 new GroupWindow(fr1Button.getText());
             }
         });
+
         JButton fr2Button = new JButton("ФР2");
+        fr2Button.setFont(new Font(fontName,fonrType,fontSize));
         fr2Button.setPreferredSize(new Dimension(400, 50));
         fr2Button.addActionListener(new ActionListener() {
             @Override
@@ -177,6 +202,7 @@ public class GroupsManagement extends BaseFrame {
         //------------------------------------------------
 
         JButton rusButton = new JButton("РУС");
+        rusButton.setFont(new Font(fontName,fonrType,fontSize));
         rusButton.setPreferredSize(new Dimension(400, 50));
         rusButton.addActionListener(new ActionListener() {
             @Override
